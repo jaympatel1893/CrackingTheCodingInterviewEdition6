ae09540e5f39643c425753e2d6a99f911ceb1936
jaythegenius48 <jaythegenius48@gmail.com>
2015-12-11 12:53:06 -0800

    First File

---
+++ Arrays And Strings/URLify.java
@@ -0,0 +1,40 @@
+import java.io.*;
+/**
+ *
+ * @author jay
+ */
+public class URLify 
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String inputString = br.readLine();
+        
+        int actualLength = Integer.parseInt(br.readLine());
+        
+        char stringToChar[] = inputString.toCharArray();
+        
+        int j = actualLength-1;
+        for(int i=stringToChar.length-1;i>=0;i--)
+        {
+            if(stringToChar[j]!=' ')
+            {
+                stringToChar[i] = stringToChar[j];
+                j--;
+            }
+            else
+            {
+                //it is a blank space
+                stringToChar[i--] = '0';
+                stringToChar[i--] = '2';
+                stringToChar[i] = '%';
+                j--;
+                
+            }
+        }
+        System.out.println(stringToChar);
+    }
+
+
+}
