44155360a4de26bb8c23e892df6889e9b119f7b9
jaythegenius48 <jaythegenius48@gmail.com>
2015-12-11 18:17:47 -0800

    PalindromePermuatation

---
+++ Arrays And Strings/PalindromePermutation.java
@@ -0,0 +1,46 @@
+import java.io.*;
+/**
+ *
+ * @author jay
+ */
+public class PalindromePermutation 
+
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String input = br.readLine();
+        //converting the input string to lower case
+        //Assuming only 26 character set, and ignoring the white space
+        input = input.toLowerCase();
+        int count[] = new int[26];
+        
+        for(int i=0;i<input.length();i++)
+        {
+            if(input.charAt(i)>=97 && input.charAt(i)<=123)
+            count[input.charAt(i)-'a']++;
+        }
+        int noOfEven = 0, noOfOdd = 0;
+        for(int i=0;i<26;i++)
+        {
+            if(count[i]%2==0)
+            {
+                noOfEven++;
+            }
+            else
+                noOfOdd++;
+        }
+        if(noOfOdd==1){
+            System.out.println("TRUE");
+        }
+        
+        else if(noOfOdd>1)
+        {
+            System.out.println("FALSE");
+        }
+        
+    }
+
+
+}
